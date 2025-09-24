f16352e1c28d2b6042f66e5f85205688c6e9f928
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-09-24 00:48:08 -0400

    Updating teams.js
    adding logos, stats, stands and schedules

---
+++ NFL_Board/src/data/teams.js
@@ -0,0 +1,260 @@
+
+export const teams = [
+    {
+        id: 1,
+        name: "Arizona Cardinals",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/c/cd/Arizona_Cardinals_logo_%282005%29.png",
+        stats:"Wins 2 - Losses 1- Ties 0", 
+        schedule: "https://www.azcardinals.com/schedule/", 
+        standing: "2nd in NFC West" 
+    },
+    {
+        id: 2,
+        name: "Atlanta Falcons",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/e/ec/Atlanta_Falcons_wordmark.svg", 
+        stats:"Wins 1 - Losses 2 - Ties 0", 
+        schedule: "https://www.atlantafalcons.com/schedule/", 
+        standing: "3rd in NFC South"
+    },
+    {
+        id: 3,
+        name: "Baltimore Ravens",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/a/a9/Baltimore_Ravens_wordmark.svg", 
+        stats:"Wins 1 - Losses 2 - Ties 0",
+        schedule: "https://www.baltimoreravens.com/schedule/", 
+        standing: "3rd in AFC North"
+    },
+    {
+        id: 4,
+        name: "Buffalo Bills",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/b/bb/Buffalo_Bills_wordmark.svg", 
+        stats:"Wins 3 - Losses 0 - Ties 0",
+        schedule: "https://www.buffalobills.com/schedule/", 
+        standing: "1st in AFC East"
+    },
+    {
+        id: 5,
+        name: "Carolina Panthers",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/d/dd/Carolina_Panthers_wordmark.svg", 
+        stats:"Wins 1 - Losses 2 - Ties 0",
+        schedule: "https://www.panthers.com/schedule/", 
+        standing: "2nd in NFC South"
+    },
+    {
+        id: 6,
+        name: "Chicago Bears",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/a/ae/Chicago_Bears_wordmark.svg", 
+        stats:"Wins 1 - Losses 2 - Ties 0",
+        schedule: "https://www.chicagobears.com/schedule/", 
+        standing: "4th in NFC North"
+    },
+    {
+        id: 7,
+        name: "Cincinnati Bengals",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/8/8e/Cincinnati_Bengals_wordmark.svg", 
+        stats:"Wins 2 - Losses 1 - Ties 0",
+        schedule: "https://www.bengals.com/schedule/", 
+        standing: "1st in AFC North"
+    },
+    {
+        id: 8,
+        name: "Cleveland Browns",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/f/f3/Cleveland_Browns_%28c._2006%29.png", 
+        stats:"Wins 1 - Losses 2- Ties 0",
+        schedule: "https://www.clevelandbrowns.com/schedule/", 
+        standing: "4th in AFC North"
+    },
+    {
+        id: 9,
+        name: "Dallas Cowboys",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/b/be/Cowboys_wordmark.svg", 
+        stats:"Wins 1 - Losses 2 - Ties 0",
+        schedule: "https://www.dallascowboys.com/schedule/", 
+        standing: "3rd in NFC East"
+    },
+    {
+        id: 10,
+        name: "Denver Broncos",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/2/25/Denver_Broncos_wordmark.svg", 
+        stats:"Wins 1 - Losses 2 - Ties 0",
+        schedule: "https://www.denverbroncos.com/schedule/", 
+        standing: "3rd in AFC West"
+    } 
+    ];
+    /**   {id: 11,
+        name: "Detroit Lions",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+    {
+        id: 12,
+        name: "Green Bay Packers",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+    {
+        id: 13,
+        name: "Houston Texans",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+    {
+        id: 14,
+        name: "Indianapolis Colts",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+    {
+        id: 15,
+        name: "Jacksonville Jaguars",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+    {
+        id: 16,
+        name: "Kansas City Chiefs",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+    {
+        id: 17,
+        name: "Las Vegas Raiders",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 18,
+        name: "Los Angeles Chargers",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 19,
+        name: "Los Angeles Rams",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 20,
+        name: "Miami Dolphins",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 21,
+        name: "Minnesota Vikings",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 22,
+        name: "New England Patriots",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 23,
+        name: "New Orleans Saints",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 24,
+        name: "New York Giants",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 25,
+        name: "New York Jets",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 26,
+        name: "Philadelphia Eagles",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 27,
+        name: "Pittsburgh Steelers",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 28,
+        name: "San Francisco 49ers",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 29,
+        name: "Seattle Seahawks",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 30,
+        name: "Tampa Bay Buccaneers",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 31,
+        name: "Tennessee Titans",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+        {
+        id: 32,
+        name: "Washington Commanders",
+        logo:, 
+        stats:, 
+        schedule:, 
+        standing:
+    },
+    */  
+
