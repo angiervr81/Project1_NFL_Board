7e2ff418ae6d020577bdf28042565a414251db4e
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-09-24 01:18:28 -0400

    Updating App.css and App.jsx

--- NFL_Board/src/App.jsx
+++ NFL_Board/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className='app'>
-      <h1>NFL_Board</h1>
+      <h1>NFL Board</h1>
 
       <div className='card-gird'>
         {teams.map((team) => (
