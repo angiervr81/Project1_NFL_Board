aed4e69ad81bf83c8cf03d95bf7143a30777ca25
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-09-23 23:32:23 -0400

    Cleanning out code

--- NFL_Board/src/components/TeamCard.jsx
+++ NFL_Board/src/components/TeamCard.jsx
@@ -1,18 +1,1 @@
-import React from 'react';
-import './App.css';
 
-const TeamCard = ({ team }) => {
-  return (
-    <div className="team-card">
-      <img src={team.logo} alt={`${team.name} logo`} className="team-logo" />
-      <h3>{team.name}</h3>
-      <p>Record: {team.record}</p>
-      <p>Next Game: {team.nextGame}</p>
-      <a href={team.statsLink} target="_blank" rel="noopener noreferrer">
-        View Stats
-      </a>
-    </div>
-  );
-}
-
-export default TeamCard;
