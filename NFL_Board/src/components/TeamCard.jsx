25d9710b7640b3fee789cb53c581626aa6f544bd
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-09-24 00:49:47 -0400

    Updated TeamCards.jsx

--- NFL_Board/src/components/TeamCard.jsx
+++ NFL_Board/src/components/TeamCard.jsx
@@ -1,1 +1,29 @@
+import React from 'react';
+import './TeamCard.css';
+
+function TeamCard({ team }) {
+  return (
+    <div className="team-card">
+      <img src={team.logo} alt={`${team.name} logo`} />
+      <h2>{team.name}</h2>
+      <p>{team.stats}</p>
+      <p>{team.standing}</p>
+
+      {/* Schedule button */}
+      {team.schedule && (
+        <a
+            href={team.schedule}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="schedule-button"
+        >
+            View Schedule
+        </a>
+)}
+
+    </div>
+  );
+}
+
+export default TeamCard;
 
