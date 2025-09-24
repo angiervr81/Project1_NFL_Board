80f1b527368ee836493c16ef07679d1ddf7f501c
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-09-24 00:55:13 -0400

    Update App.jsx
    adding imports

---
+++ NFL_Board/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { teams } from './data/teams.js';
+import TeamCard from './components/TeamCard.jsx';
+import './App.css';
+function App() {
+  return (
+    <div className='app'>
+      <h1>NFL_Board</h1>
+
+      <div className='card-gird'>
+        {teams.map((team) => (
+          <TeamCard key={team.id} team={team} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
